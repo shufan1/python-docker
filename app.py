e2b7854d667f710fd89d5a35d45efcaec0aa655b
Shufan Xia <39500675+shufan1@users.noreply.github.com>
2021-09-19 23:45:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,85 +19,85 @@ def hello(name=None):
     ID = np.random.randint(100)
     return render_template('hello.html', name = name, ID = ID)
 
-@app.route('/initdb')
-def db_init():
-    # make Database: location
-    mydb = mysql.connector.connect(
-    host="mysqldb",
-    user="root",
-    password="p@ssword1"
-    )
-    cursor = mydb.cursor()
+# @app.route('/initdb')
+# def db_init():
+#     # make Database: location
+#     mydb = mysql.connector.connect(
+#     host="mysqldb",
+#     user="root",
+#     password="p@ssword1"
+#     )
+#     cursor = mydb.cursor()
     
-    cursor.execute("DROP DATABASE IF EXISTS location")
-    cursor.execute("CREATE DATABASE location")
-    cursor.close()
+#     cursor.execute("DROP DATABASE IF EXISTS location")
+#     cursor.execute("CREATE DATABASE location")
+#     cursor.close()
     
-    #make Table: cities 
-    mydb = mysql.connector.connect(
-    host="mysqldb",
-    user="root",
-    password="p@ssword1",
-    database="location"
-    )
-    cursor = mydb.cursor()
+#     #make Table: cities 
+#     mydb = mysql.connector.connect(
+#     host="mysqldb",
+#     user="root",
+#     password="p@ssword1",
+#     database="location"
+#     )
+#     cursor = mydb.cursor()
     
-    cursor.execute("DROP TABLE IF EXISTS cities")
-    cursor.execute("CREATE TABLE cities (name VARCHAR(255), long_and_lat VARCHAR(255));")
-    cursor.execute("INSERT INTO cities VALUES ('San Francisco', '(-194.0, 53.0)');")
-    cursor.execute("INSERT INTO cities VALUES ('Springfield',	'(39.8,	-89.7);')")
-    cursor.close()
+#     cursor.execute("DROP TABLE IF EXISTS cities")
+#     cursor.execute("CREATE TABLE cities (name VARCHAR(255), long_and_lat VARCHAR(255));")
+#     cursor.execute("INSERT INTO cities VALUES ('San Francisco', '(-194.0, 53.0)');")
+#     cursor.execute("INSERT INTO cities VALUES ('Springfield',	'(39.8,	-89.7)');")
+#     cursor.close()
     
-    return 'init database'
+#     return 'init database'
 
 @app.route('/cities')
 def get_cities() :
-  mydb = mysql.connector.connect(
+    mydb = mysql.connector.connect(
     host="mysqldb",
     user="root",
     password="p@ssword1",
     database="location"
-  )
-  cursor = mydb.cursor()
-  
-  
-  cursor.execute("SELECT * FROM cities")
-  
-  row_headers=[x[0] for x in cursor.description] #this will extract row headers
-  
-  results = cursor.fetchall()
-  json_data=[]
-  for result in results:
+    )
+    cursor = mydb.cursor()
+
+
+    cursor.execute("SELECT * FROM cities")
+
+    row_headers=[x[0] for x in cursor.description] #this will extract row headers
+
+    results = cursor.fetchall()
+    json_data=[]
+    for result in results:
     json_data.append(dict(zip(row_headers,result)))
-  
-  cursor.close()
-  
-  return json.dumps(json_data)
+
+    cursor.close()
+
+    return json.dumps(json_data)
   
   
 @app.route('/timezones')
 def get_timezones() :
-  mydb = mysql.connector.connect(
+    mydb = mysql.connector.connect(
     host="mysqldb",
     user="root",
     password="p@ssword1",
     database="mysql"
-  )
-  cursor = mydb.cursor()
+    )
+    cursor = mydb.cursor()
 
 
-  cursor.execute("SELECT * FROM time_zone_name LIMIT 10;")
+    cursor.execute("SELECT * FROM time_zone_name LIMIT 10;")
 
-  row_headers=[x[0] for x in cursor.description] #this will extract row headers
+    row_headers=[x[0] for x in cursor.description] #this will extract row headers
 
-  results = cursor.fetchall()
-  json_data=[]
-  for result in results:
+    results = cursor.fetchall()
+    json_data=[]
+    for result in results:
     json_data.append(dict(zip(row_headers,result)))
 
-  cursor.close()
+    cursor.close()
 
-  return json.dumps(json_data)
+    return json.dumps(json_data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080)
