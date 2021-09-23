aa6dc84c50477d988649d6d8922a2e2b53049cf4
EC2 Default User <ec2-user@ip-172-31-18-42.us-east-2.compute.internal>
2021-09-23 01:46:56 +0000

    Test CI

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def hello(name=None):
     ID = np.random.randint(100)
     return render_template('hello.html', name = name, ID = ID)
 
-@app.route('/initdb')
+@app.route('/initcitydb')
 def db_init():
     # make Database: location
     mydb = mysql.connector.connect(
@@ -28,7 +28,7 @@ def db_init():
     password="p@ssword1"
     )
     cursor = mydb.cursor()
-    
+    print("here")
     cursor.execute("DROP DATABASE IF EXISTS location")
     cursor.execute("CREATE DATABASE location")
     cursor.close()
@@ -41,14 +41,24 @@ def db_init():
     database="location"
     )
     cursor = mydb.cursor()
-    
     cursor.execute("DROP TABLE IF EXISTS cities")
     cursor.execute("CREATE TABLE cities (name VARCHAR(255), long_and_lat VARCHAR(255));")
-    cursor.execute("INSERT INTO cities VALUES ('San Francisco', '(-194.0, 53.0)');")
-    cursor.execute("INSERT INTO cities VALUES ('Springfield',	'(39.8,	-89.7)');")
+    cursor.execute("INSERT INTO cities (name,long_and_lat) VALUES ('San Francisco', '(-194.0, 53.0)');")
+    print("here2")
+    cursor.execute("INSERT INTO cities (name,long_and_lat) VALUES ('Springfield',	'(39.8,	-89.7)');")
+    
+    cursor.execute("SELECT * FROM cities;")
+    
+    row_headers=[x[0] for x in cursor.description]  
+    results = cursor.fetchall()
+    json_data=[]
+    for result in results:
+      json_data.append(dict(zip(row_headers,result)))
+    
     cursor.close()
     
-    return 'init database'
+    return json.dumps(json_data)
+      
 
 @app.route('/cities')
 def get_cities() :
@@ -59,10 +69,9 @@ def get_cities() :
     database="location"
   )
   cursor = mydb.cursor()
+  cursor.execute("SELECT * FROM cities;")
   
-  
-  cursor.execute("SELECT * FROM cities")
-  
+
   row_headers=[x[0] for x in cursor.description] #this will extract row headers
   
   results = cursor.fetchall()
@@ -73,6 +82,23 @@ def get_cities() :
   cursor.close()
   
   return json.dumps(json_data)
+
+@app.route('/addcity/<city>/<long>/<lat>')
+def add_city(city=None, long=None,lat=None):
+  mydb = mysql.connector.connect(
+    host="mysqldb",
+    user="root",
+    password="p@ssword1",
+    database="location"
+  )
+  
+  long_and_lat ="("+str(long)+","+ str(lat)+")"
+  
+  cursor = mydb.cursor()
+  cursor.execute("INSERT INTO cities (name,long_and_lat) VALUES ('{0}', '{1}');".format(city,long_and_lat))
+  cursor.close()
+  return '{"new entry":[{"city":{0},"long_lat":{1}}]}'.format(city,long_and_lat)
+    
   
   
 @app.route('/timezones')
